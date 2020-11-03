599e03122212e11141cc0d8b59e35e7934968be9
Mason Phillips <mjphillips2000@gmail.com>
2020-11-03 20:30:21 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,10 +6,11 @@ class Backer
   end
   
   def backed_projects
+    @backed_projects = []
   end
   
   def back_project(project)
-    @backed_projects << project
+    backed_projects << project
     project.backers << self
   end
 end
