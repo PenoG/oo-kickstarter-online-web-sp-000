4648115128d039b9c4a1c100b22b0986d908a435
Mason Phillips <mjphillips2000@gmail.com>
2020-11-03 20:28:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,16 @@
 class Backer
-  attr_reader :name, :backed_projects
+  attr_accessor :backed_projects
+  attr_reader :name
   
   def initialize(name)
     @name = name
-    @backed_projects = []
+  end
+  
+  def backed_projects
   end
   
   def back_project(project)
     @backed_projects << project
+    project.backers << self
   end
 end
